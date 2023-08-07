e3cb6e56a7dd66ca291c562d2cfe20c46a05c86c
ArinaBogan <arina.bogan@gmail.com>
2023-08-07 16:08:03 +0300

    added all

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const num = Math.round(Math.random() * 100);
 
-
 function searchNumber(n) {
-
     let start = 0;
     let end = 100;
     let count = 0;
 
     for (let i = start; i < end; i++) {
-        let value = prompt('Программа загадала число.Ваша задача угадать его.Введите чило от 0 до 100');
+        let value = +prompt(`Программа загадала число.Ваша задача угадать его.Введите чило от ${start} до ${end}`);
         count++
-        if (n == value) {
-            console.log('Вы угадали число,поздравляем!');
-            break;
-        } else if (n > value) {
-            start = value;
-            alert('Слишком много, попробуйте еще раз');
-        } else if (n < value) {
-            end = value;
-            alert('Слишком мало, попробуйте еще раз');
+        if (!isNaN(value) && value < 101 && value > 0) {
+            if (n == value) {
+                alert(`Вы угадали число с ${count} попытки,поздравляем!`);
+                break;
+            } else if (n > value) {
+                start = value;
+                alert('Слишком мало, попробуйте еще раз');
+            } else if (n < value) {
+                end = value;
+                alert('Слишком много, попробуйте еще раз');
+            }
+        } else {
+            alert('Неверное значение.Введите число!');
         }
+
     }
 }
 let result = searchNumber(num);
