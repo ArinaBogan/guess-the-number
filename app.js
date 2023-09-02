1b8372f6b3798ccae4c6e432825be7734746b3d4
ArinaBogan <arina.bogan@gmail.com>
2023-09-03 00:31:18 +0300

    added oop

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const num = Math.round(Math.random() * 100);
+class Guess {
+    searchNumber(num) {
+        try {
+            let start = 0;
+            let end = 100;
+            let count = 0;
 
-function searchNumber(n) {
-    let start = 0;
-    let end = 100;
-    let count = 0;
-
-    for (let i = start; i < end; i++) {
-        let value = +prompt(`Программа загадала число.Ваша задача угадать его.Введите чило от ${start} до ${end}`);
-        count++
-        if (!isNaN(value) && value < 101 && value > 0) {
-            if (n == value) {
-                alert(`Вы угадали число с ${count} попытки,поздравляем!`);
-                break;
-            } else if (n > value) {
-                start = value;
-                alert('Слишком мало, попробуйте еще раз');
-            } else if (n < value) {
-                end = value;
-                alert('Слишком много, попробуйте еще раз');
+            for (let i = start; i < end; i++) {
+                let value = +prompt(`Программа загадала число.Ваша задача угадать его.Введите чило от ${start} до ${end}`);
+                count++
+                if (!isNaN(value) && value < 101 && value > 0) {
+                    if (num == value) {
+                        alert(`Вы угадали число с ${count} попытки,поздравляем!`);
+                        break;
+                    } else if (num > value) {
+                        start = value;
+                        alert('Слишком мало, попробуйте еще раз');
+                    } else if (num < value) {
+                        end = value;
+                        alert('Слишком много, попробуйте еще раз');
+                    }
+                } else {
+                    alert('Неверное значение.Введите число!');
+                }
             }
-        } else {
-            alert('Неверное значение.Введите число!');
+        } catch (error) {
+            return (error.message)
         }
-
     }
 }
-let result = searchNumber(num);
-console.log(result);
+const num = Math.round(Math.random() * 100);
+
+const guess = new Guess();
+guess.searchNumber(num)
